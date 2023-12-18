fb2dc2bd7a75c0ce24ae61950f0f738d466d0821
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-12-18 13:06:31 -0500

    005-after lecture

--- 05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react"; //1. import useEffect
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
@@ -7,19 +7,42 @@ const App = () => {
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
 
-  const handleClick = () => {
+  console.log(" ------ component rendering ----- ")
+  
+  useEffect(() => { //2. callback function as the first argument for the useEffect hook
+    console.log(" ------ side effect, useEffect rendering ----- ")
     fetch("http://localhost:4000/projects")
-      .then((res) => res.json())
-      .then((projects) => setProjects(projects));
-  };
+      .then(response => response.json())
+      .then(data => setProjects(data))
+  }, []) //3. add an empty dependency array here as the second argument 
+
+  // const Timer = () => {
+  //   const [ count, setCount ] = useState(0)
+
+  //   useEffect(() => {
+  //     const timer = setTimeout(() => {
+  //       setCount((prev) => prev + 1)
+  //     }, 1000)
+
+  //     return () => {
+  //       clearTimeout(timer)
+  //     }
+  //   })
+  // }
+
+  const onAddProject = ( newProject ) => {
+    setProjects([...projects, newProject])
+    //spread operator is to add the old projects array and the new project 
+  }
+
 
   const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
 
   return (
     <div className={isDarkMode ? "App" : "App light"}>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
-      <ProjectForm />
-      <button onClick={handleClick}>Load Projects</button>
+      <ProjectForm onAddProject = { onAddProject }/>
+      {/* <button onClick={handleClick}>Load Projects</button> */}
       <ProjectList projects={projects} />
     </div>
   );
--- 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectForm.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/components/ProjectForm.js
@@ -1,19 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ProjectForm = () => {
+const ProjectForm = ( { onAddProject }) => {
+
+  const initialForm = {
+    name: "",
+    about: "",
+    phase: 0,
+    link: "",
+    image: ""
+  }
+  
+  const [ formData, setFormData ] = useState(initialForm)
+
+  const handleOnChange = (e) => {
+    console.log(e.target)
+    console.log(e.target.name)
+    console.log(e.target.value)
+
+    //object de-structuring
+    const { name, value } = e.target
+
+    console.log(name, value)
+
+    // setFormData((formData) => ({
+    //   ...formData, ///spread the previous data 
+    //   [name]: value // add the new name and the value of the input 
+    // })) //why using the square bracket? bc this is the key
+
+    setFormData({...formData, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    fetch("http://localhost:4000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data)) //pessimistic rendering: using the response back from the server
+
+    onAddProject(formData) //optimistic. why? it's not using the response back from the server
+    //formData is the new project 
+
+    //clear the form
+    setFormData(initialForm)
+
+  }
+
+  console.log(formData)
   return (
     <section>
-      <form className="form" autoComplete="off">
+      <form className="form" autoComplete="off" onSubmit={handleSubmit}>
         <h3>Add New Project</h3>
 
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input 
+            type="text" 
+            id="name" 
+            name="name" 
+            onChange={handleOnChange}
+            value={formData.name}/>
 
         <label htmlFor="about">About</label>
-        <textarea id="about" name="about" />
+        <textarea 
+            id="about" 
+            name="about"
+            onChange={handleOnChange} 
+            value={formData.about}/>
 
         <label htmlFor="phase">Phase</label>
-        <select name="phase" id="phase">
+        <select 
+            name="phase" 
+            id="phase"
+            onChange={handleOnChange}
+            value={formData.phase}>
           <option>Select One</option>
           <option value="1">Phase 1</option>
           <option value="2">Phase 2</option>
@@ -23,10 +87,20 @@ const ProjectForm = () => {
         </select>
 
         <label htmlFor="link">Project Homepage</label>
-        <input type="text" id="link" name="link" />
+        <input 
+          type="text" 
+          id="link" 
+          name="link" 
+          onChange={handleOnChange}
+          value={formData.link}/>
 
         <label htmlFor="image">Screenshot</label>
-        <input type="text" id="image" name="image" />
+        <input 
+          type="text" 
+          id="image" 
+          name="image" 
+          onChange={handleOnChange}
+          value={formData.image}/>
 
         <button type="submit">Add Project</button>
       </form>
